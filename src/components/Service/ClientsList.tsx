79fdceb71ebbd0c500fbd64a32ed4f4c1898581b
Shyam Pankhaniya <shyam.j.pankhaniya05@gmail.com>
2024-09-29 17:45:11 +0530

    get clients in service page

--- src/components/Service/ClientsList.tsx
+++ src/components/Service/ClientsList.tsx
@@ -1,19 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useGetClients } from './useGetClients';
 import { useParams } from 'react-router-dom';
 
 function ClientsList() {
-  // const { isPending, isSuccess, data } = useGetClients();
-  const { id } = useParams();
+  const { id, sid } = useParams();
+  const [clientlist, setClientList] = useState([]);
+  const param = {
+    userId: id ? parseInt(id) : 0,
+    serviceId: sid ? parseInt(sid) : 0,
+  };
+  const { isPending, isSuccess, data } = useGetClients(param);
   useEffect(() => {
-    console.log(id);
+    setClientList(data?.data.data);
   });
   {
     if (isPending) {
       return <h1>Loading.....</h1>;
     }
   }
-  return <div>list of clients</div>;
+  return (
+    <div>
+      list of clients
+      {isSuccess && (
+        <ul>{clientlist && clientlist.map((c) => <h1>{c.userId}</h1>)}</ul>
+      )}
+    </div>
+  );
 }
 
 export default ClientsList;
